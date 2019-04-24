d4d65612a99a2ec07e2e315f98c9b968ecd52177
Omri Bornstein <omribor@gmail.com>
2019-04-24 10:30:27 +0930

    replced recursive async function clickNext for do-while loop

--- index.ts
+++ index.ts
@@ -1,69 +1,90 @@
 import {Browser, Page, launch} from "puppeteer";
 import {get} from "https";
-import {createWriteStream, WriteStream} from "fs";
+import {createWriteStream, unlink} from "fs";
 import {Answers, prompt} from "inquirer";
+import { async } from "rxjs/internal/scheduler/async";
 
 var srcs: string[] = [];
 const userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36";
 var count = 0;
-var i = 0;
+var I = 0;
 
-async function auth() {
+async function init() {
 	const answers: Answers = await prompt([{type: "input", name: "username"}, {type: "password", name: "password"}, {type: "input", name: "id"}]);
-	// console.log(answers);
-	scrape(answers.username, answers.password, answers.id);
+	beginScrape(answers.username, answers.password, answers.id);
 }
 
-async function scrape(username: string, password: string, id: string) {
+async function beginScrape(username: string, password: string, id: string) {
 	try {
 		const browser = await launch({headless: false, devtools: true});
-		const page = await browser.newPage();
+		const page = (await browser.pages())[0];
 		await page.setUserAgent(userAgent);
 		await page.goto('https://www.instagram.com/accounts/login/');
 		await page.waitForSelector('input[name="username"]');
 		await page.type('input[name="username"]', username);
 		await page.type('input[name="password"]', password);
 		await page.click('button[type="submit"]');
+		await page.waitFor(2000);
 		await page.waitForSelector("img._6q-tv");
 		await page.goto(`https://www.instagram.com/p/${id}`);
-		await page.waitForSelector('div.ZyFrc');
-		//video.tWeCl
-		clickNext(browser, page, id);
+		await page.waitForSelector('div.ZyFrc', {visible: true});
+		detectFiles(browser, page, id);
 	} catch (error) { console.error(error.message) }
 }
-async function clickNext(browser: Browser, page: Page, id: string) {
+async function detectFiles(browser: Browser, page: Page, id: string) {
 	try {
-		const videosDuplicates = await page.$$eval('video[src].tWeCl', videos => videos.map(video => video.getAttribute('src')));
-		const imagesDuplicates = await page.$$eval('img[src].FFVAD', images => images.map(image => image.getAttribute('src')));
-		imagesDuplicates.forEach(duplicate => srcs.push(duplicate));
-		videosDuplicates.forEach(duplicate => srcs.push(duplicate));
-		await page.click("div.coreSpriteRightChevron");
-		clickNext(browser, page, id);
+		var nextButtons = await page.$("div.coreSpriteRightChevron");
+		do {
+			const videosDuplicates = await page.$$eval('video[src].tWeCl', videos => videos.map(video => video.getAttribute('src')));
+			const imagesDuplicates = await page.$$eval('img[src].FFVAD', images => images.map(image => image.getAttribute('src')));
+			imagesDuplicates.forEach(duplicate => srcs.push(duplicate));
+			videosDuplicates.forEach(duplicate => srcs.push(duplicate));
+			await page.click("div.coreSpriteRightChevron");
+			nextButtons = await page.$("div.coreSpriteRightChevron");
+		} while (nextButtons !== null);
+		organiseFiles(browser, id);
+		return;
 	} catch (error) {
-		const URLs = Array.from(new Set(srcs));//[...new Set<string>(srcs)];
-		count = URLs.length;
-		for (let i = 0; i < URLs.length; i++) {
-			const url = URLs[i];
-			downloadFile(browser, url, id);
-		}
+		console.error(error.message);
+		organiseFiles(browser, id);
 		return;
 	}
 }
-async function downloadFile(browser: Browser, URL: string, id: string) {
+function organiseFiles(browser: Browser, id: string) {
+	console.log("Found files.");
+	const URLs = Array.from(new Set(srcs));
+	count = URLs.length;
+	for (let i = 0; i < URLs.length; i++) {
+		const url = URLs[i];
+		console.log(url);
+		if (url.includes(".mp4")) downloadFile(browser, url, id, `${process.cwd()}/${I}-${id}.mp4`);
+		if (url.includes(".jpg")) downloadFile(browser, url, id, `${process.cwd()}/${I}-${id}.jpg`);
+	}
+	return;
+}
+
+async function downloadFile(browser: Browser, URL: string, id: string, path: string) {
 	try {
-		const page2 = await browser.newPage();
-		await page2.setUserAgent(userAgent);
-		await page2.goto(URL, { waitUntil: "networkidle0" });
-		// fs.mkdir(`${__dirname}/${id}`, {}, (error) => {if (error) throw error});
-		var file: WriteStream;
-		if (URL.includes(".mp4")) file = createWriteStream(`${process.cwd()}/${i}-${id}.mp4`);
-		if (URL.includes(".jpg")) file = createWriteStream(`${process.cwd()}/${i}-${id}.jpg`);
-		get(URL, (res) => res.pipe(file));
-		console.log(URL);
-		i += 1;
-		console.log(i);
-		if (i === count) await browser.close();
+		var file = createWriteStream(path);
+		const request = get(URL, (response) => {
+			if (response.statusCode !== 200) return console.error("Download failed.");
+			const length = parseInt(response.headers['content-length'], 10);
+			var current = 0;
+			response.on("end", () => console.log("Download ended.")).pipe(file);
+			response.on("data", chunk => {
+				current += chunk.length;
+				console.log(`Downloaded %${(100 * current / length).toFixed(2)}`);
+			});
+			// response.pipe(file);
+		});
+		file.on("finish", () => file.close());
+		request.on("error", error => {
+			unlink(path, null);
+			console.error(error.message);
+		});
+		I += 1;
+		if (I === count) await browser.close();
 		return;
 	} catch (error) { console.error(error.message) }
 }
-auth();
+init();
