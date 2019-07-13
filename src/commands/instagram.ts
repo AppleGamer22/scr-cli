194fe386e521cf98ddbae4ad37ed043101237bf8
Omri Bornstein <omribor@gmail.com>
2019-07-13 22:01:47 +0930

    better test script refactored Instagram class

--- src/commands/instagram.ts
+++ src/commands/instagram.ts
@@ -16,104 +16,97 @@ export default class Instagram extends Command {
 	startScarpingTime = 0;
 	fileCount = 0;
 	currentFileIndex = 0;
-	readonly userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36";
 
 	async run() {
 		config({path: `${__dirname}/../../.env`});
 		const {INSTAGRAM} = process.env;
 		if (!JSON.parse(INSTAGRAM!)) {
-			console.log("Please make sure that you enter the correct Instagram creditentials and post ID...");
-			const username: string = await cli.prompt("username", {type: "normal", prompt: "Your Instagram username: "});
-			const password: string = await cli.prompt("password", {type: "hide", prompt: "Your Instagram password: "});
-			const id: string = await cli.prompt("id", {type: "normal", prompt: "Instagram post ID (after https://www.instagram.com/p/): "});
-			const background: boolean = await cli.confirm("Do you want to see the browser?");
-			await this.beginScrape(id, !background);
+			console.error("You are not authenticated.");
 		} else if (JSON.parse(INSTAGRAM!)) {
-			const {args, flags} = this.parse(Instagram);
-			await this.beginScrape(args.post, flags.headless);
+			try {
+				const {args, flags} = this.parse(Instagram);
+				const {browser, page} = (await beginScrape(userAgent, flags.headless))!
+				const urls = [...(new Set<string>(await detectFiles(browser, page, args.post)))];
+				for (const url of urls) {
+					if (url.includes(".jpg")) await this.downloadFile(browser, url, ".jpg");
+					if (url.includes(".mp4")) await this.downloadFile(browser, url, ".mp4");
+				}
+			} catch (error) { console.error(error.message); }
 		}
 	}
 
-	async beginScrape(id: string, background: boolean) {
-		cli.action.start("Opening Puppeteer...");
-		try {
-			const browser = await launch({
-				headless: background,
-				userDataDir: `${__dirname}/../../Chrome`,
-				devtools: !background,
-				defaultViewport: null
-			});
-			this.startScarpingTime = Date.now();
-			cli.action.stop();
-			await browser.browserContexts()[0].overridePermissions("https://www.instagram.com", ["notifications"]);
-			const page = (await browser.pages())[0];
-			await page.setUserAgent(this.userAgent);
-			cli.action.start("Searching for files...");
-			await this.detectFiles(browser, page, id);
-		} catch (error) { console.error(error.message); }
-	}
-	async detectFiles(browser: Browser, page: Page, id: string) {
-		try {
-			await page.goto(`https://www.instagram.com/p/${id}`, {waitUntil: "domcontentloaded"});
-			const errorLabelSelector = "body > div > div.page.-cx-PRIVATE-Page__body.-cx-PRIVATE-Page__body__ > div > div";
-			if ((await page.$("div.error-container")) !== null) {
-				console.error(`Failed to find post ${id}`);
-				await browser.close();
-				return;
-			}
-			await page.waitForSelector("div.ZyFrc", {visible: true});
-			var nextButtons = await page.$("div.coreSpriteRightChevron");
-			do {
-				const videosDuplicates = await page.$$eval("video.tWeCl", videos => videos.map(video => video.getAttribute("src")));
-				const imagesDuplicates = await page.$$eval("img.FFVAD", images => images.map(image => image.getAttribute("src")));
-				imagesDuplicates.forEach(duplicate => { if (duplicate) this.srcs.push(duplicate); });
-				videosDuplicates.forEach(duplicate => { if (duplicate) this.srcs.push(duplicate); });
-				await page.click("div.coreSpriteRightChevron");
-				nextButtons = await page.$("div.coreSpriteRightChevron");
-			} while (nextButtons !== null);
-			this.organiseFiles(browser, id);
-		} catch (error) {
-			if (error.message === "No node found for selector: div.coreSpriteRightChevron") await this.organiseFiles(browser, id);
-		}
-	}
-	async organiseFiles(browser: Browser, id: string) {
-		cli.action.stop();
-		try {
-			const URLs = [...(new Set<string>(this.srcs))];
-			this.fileCount = URLs.length;
-			for (const url of URLs) {
-				if (url.includes(".jpg")) await this.downloadFile(browser, url, ".jpg");
-				if (url.includes(".mp4")) await this.downloadFile(browser, url, ".mp4");
-			}
-		} catch (error) { console.error(error); }
-	}
-
-	async downloadFile(browser: Browser, URL: string, fileType: string) {
+	downloadFile(browser: Browser, URL: string, fileType: ".jpg" | ".mp4") {
 		const path = `${process.cwd()}/${basename(URL).split("?")[0]}`
-		try {
+		return new Promise(async (resolve, reject) => {
 			console.log(`${fileType} ${this.currentFileIndex + 1}\n${URL}`);
-			cli.action.start("Download began.");
 			var file = createWriteStream(path);
+			cli.action.start("Download began.");
 			const request = get(URL, response => {
 				if (response.statusCode !== 200) throw console.error("Download failed.");
 				response.on("end", () => cli.action.stop()).pipe(file);
-				// const length = parseInt(response.headers['content-length'], 10);
-				// var current = 0;
-				// response.on("data", chunk => {
-				// 	current += chunk.length;
-				// 	console.log(`Downloaded %${(100 * current / length).toFixed(2)}`);
-				// });
 			});
-			file.on("finish", () => file.close());
+			file.on("finish", () => {
+				cli.action.stop();
+				file.close();
+				resolve();
+			});
 			request.on("error", error => {
 				unlink(path, null!);
 				console.error(error.message);
+				reject();
 			});
 			this.currentFileIndex += 1;
 			if (this.currentFileIndex === this.fileCount && this.startScarpingTime !== 0) {
 				await browser.close();
 				console.log(`Scrape time: ${(Date.now() - this.startScarpingTime)/1000}s`);
 			}
-		} catch (error) { console.error(error.message); }
+		});
 	}
+}
+
+export const userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36";
+
+export async function beginScrape(userAgent: string, background: boolean): Promise<{browser: Browser, page: Page} | undefined> {
+	cli.action.start("Opening Puppeteer...");
+	try {
+		const browser = await launch({
+			headless: background,
+			userDataDir: `${__dirname}/../../Chrome`,
+			devtools: !background,
+			defaultViewport: null
+		});
+		cli.action.stop();
+		const page = (await browser.pages())[0];
+		await page.setUserAgent(userAgent);
+		cli.action.start("Searching for files...");
+		return {browser, page};
+	} catch (error) { console.error(error.message); }
+}
+
+export async function detectFiles(browser: Browser, page: Page, id: string): Promise<string[]> {
+	cli.action.start("Searching for files...");
+	var srcs: string[] = [];
+	try {
+		await page.goto(`https://www.instagram.com/p/${id}`, {waitUntil: "domcontentloaded"});
+		const errorLabelSelector = "body > div > div.page.-cx-PRIVATE-Page__body.-cx-PRIVATE-Page__body__ > div > div";
+		if ((await page.$("div.error-container")) !== null) {
+			console.error(`Failed to find post ${id}`);
+			await browser.close();
+		}
+		await page.waitForSelector("div.ZyFrc", {visible: true});
+		var nextButtons = await page.$("div.coreSpriteRightChevron");
+		do {
+			const videosDuplicates = await page.$$eval("video.tWeCl", videos => videos.map(video => video.getAttribute("src")));
+			const imagesDuplicates = await page.$$eval("img.FFVAD", images => images.map(image => image.getAttribute("src")));
+			imagesDuplicates.forEach(duplicate => { if (duplicate) srcs.push(duplicate); });
+			videosDuplicates.forEach(duplicate => { if (duplicate) srcs.push(duplicate); });
+			await page.click("div.coreSpriteRightChevron");
+			nextButtons = await page.$("div.coreSpriteRightChevron");
+		} while (nextButtons !== null);
+		cli.action.stop();
+	} catch (error) {
+		return [...(new Set<string>(srcs))!];
+	}
+	await browser.close();
+	return [...(new Set<string>(srcs))!];
 }
--- test/commands/instagram.test.ts
+++ test/commands/instagram.test.ts
@@ -1,11 +1,15 @@
 import { expect, test } from "@oclif/test";
+import { execSync, exec } from "child_process";
+import {beginScrape, detectFiles, userAgent} from "./../../src/commands/instagram";
 
-describe("hello", () => {
-	test.stdout().command(["hello"]).it("runs hello", ctx => {
-		expect(ctx.stdout).to.contain("hello world");
-	});
-
-	test.stdout().command(["hello", "--name", "jeff"]).it("runs hello --name jeff", ctx => {
-		expect(ctx.stdout).to.contain("hello jeff");
+describe("Instagram", () => {
+	test.timeout(10000).it("scrapes Bz2MPhPhOQu & gets to JPEGs", async (context, done) => {
+		try {
+			const {browser, page} = (await beginScrape(userAgent, false))!;
+			const urls = await detectFiles(browser, page, "BtqB_I6FWNC");
+			console.log(urls);
+			done();
+			for (const url of urls) expect(url).to.include(".jpg");
+		} catch (error) { console.error(error.message); }
 	});
 });
