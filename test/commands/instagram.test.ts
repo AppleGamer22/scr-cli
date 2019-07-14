4b84d780fdebae0a856efcbec49494b28d92a7e9
Omri Bornstein <omribor@gmail.com>
2019-07-14 13:33:10 +0930

    more efficient browser initialisation

--- test/commands/instagram.test.ts
+++ test/commands/instagram.test.ts
@@ -1,15 +1,25 @@
 import {expect, test} from "@oclif/test";
+import {Browser, Page} from "puppeteer";
 import {beginScrape, detectFiles, userAgent} from "./../../src/commands/instagram";
 
 describe("Instagram", () => {
-	test.timeout(6000).it("scrapes Bz2MPhPhOQu & gets to 1 JPEG", async (context, done) => {
+	let browser: Browser, page: Page;
+	beforeEach(async () => {
 		try {
-			const {browser, page} = (await beginScrape(userAgent, false))!;
+			const puppeteerSuite = (await beginScrape(userAgent, false))!;
+			browser = puppeteerSuite.browser;
+			page = puppeteerSuite.page;
+		} catch (error) { console.error(error.message); }
+	});
+	test.timeout(6000).it("scrapes Bz2MPhPhOQu & gets 1 JPEG", async (_, done) => {
+		try {
 			const urls = await detectFiles(browser, page, "Bz2MPhPhOQu");
 			await browser.close();
-			console.log(urls);
 			done();
-			for (const url of urls) expect(url).to.include(".jpg");
+			expect(urls.length).to.equal(1);
+			console.log(urls[0]);
+			expect(urls[0]).to.include(".jpg");
+			expect(urls[0]).to.include("cdninstagram.com");
 		} catch (error) { console.error(error.message); }
 	});
 });
