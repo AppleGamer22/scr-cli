bc0029f64299be5f5d989b97b5a66dc7239a99a5
Omri Bornstein <omribor@gmail.com>
2019-10-07 17:22:19 +1030

    new instagram post algorithm

--- test/commands/instagram.test.ts
+++ test/commands/instagram.test.ts
@@ -12,10 +12,10 @@ describe("Instagram", () => {
 			page = puppeteerSuite.page;
 		} catch (error) { console.error(error.message); }
 	});
+	afterEach(async () => await browser.close());
 	test.timeout(6000).it("scrapes Bz2MPhPhOQu & gets 1 public JPEG", async (_, done) => {
 		try {
-			const urls = await detectFiles(browser, page, "Bz2MPhPhOQu");
-			await browser.close();
+			const urls = (await detectFiles(browser, page, "Bz2MPhPhOQu"))!;
 			done();
 			expect(urls.length).to.equal(1);
 			console.log(urls[0]);
@@ -26,14 +26,28 @@ describe("Instagram", () => {
 	});
 	test.timeout(6000).it("scrapes B2VA_gNg2EQ & gets 1 public MP4", async (_, done) => {
 		try {
-			const urls = await detectFiles(browser, page, "B2VA_gNg2EQ");
-			await browser.close();
+			const urls = (await detectFiles(browser, page, "B2VA_gNg2EQ"))!;
+			done();
+			expect(urls.length).to.equal(1);
+			console.log(urls[0]);
+			expect(urls[0]).to.include("https://");
+			expect(urls[0]).to.include(".mp4");
+			expect(urls[0]).to.include("cdninstagram.com");
+		} catch (error) { console.error(error.message); }
+	});
+	test.timeout(6000).it("scrapes BqWkJemlFJb & gets a public MP4 & a public JPEG", async (_, done) => {
+		try {
+			const urls = (await detectFiles(browser, page, "BqWkJemlFJb"))!;
 			done();
 			expect(urls.length).to.equal(2);
 			console.log(urls[0]);
 			expect(urls[0]).to.include("https://");
 			expect(urls[0]).to.include(".mp4");
 			expect(urls[0]).to.include("cdninstagram.com");
+			console.log(urls[1]);
+			expect(urls[1]).to.include("https://");
+			expect(urls[1]).to.include(".jpg");
+			expect(urls[1]).to.include("cdninstagram.com");
 		} catch (error) { console.error(error.message); }
 	});
 });
